f6c87c5f791fb69cd318733f462f29543b95b4d4
Michael Zhou <mzhou08@gmail.com>
2021-04-14 23:36:38 -0400

    truncating code done need to configure interpreter and other stuff

--- process_data.py
+++ process_data.py
@@ -3,4 +3,12 @@ import pandas as pd
 import re
 
 fakeDB = pd.read_csv("Fake.csv")
-realDB = pd.read_csv(".csv")
+realDB = pd.read_csv("True.csv")
+
+mnrows = min(fakeDB.shape()[0], realDB.shape()[0])
+fakeDB = fakeDB.truncate(after=mnrows - 1)
+realDB = realDB.truncate(after=mnrows - 1)
+
+for string in clear
+start_header = "\w+ - \(\w+\)"
+re.sub(start_header, "", string)
