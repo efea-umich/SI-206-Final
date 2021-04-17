a5814a6ce8c4398d1853521396ac347c4766fbf5
Michael Zhou <mzhou08@gmail.com>
2021-04-17 00:16:42 -0400

    fixed make_wordcloud, now I can make two separate wordclouds.

--- make_wordcloud.py
+++ make_wordcloud.py
@@ -8,14 +8,19 @@ dp = DataProcessor()
 
 real, fake = dp.getDatasets()
 
-#realString = "".join([art for art in real])
 fakeString = "".join([art for art in fake])
-
-#print(realString == fakeString)
 
 wc = WordCloud(stopwords=STOPWORDS, height=1080, width=1920)
 
-#r_wc = wc.generate(realString)
 f_wc = wc.generate(fakeString)
-#r_wc.to_file('static/visualizations/realWordCloud.png')
+
 f_wc.to_file('static/visualizations/fakeWordCloud.png')
+
+#Moving onto generate a wordcloud for the real articles
+realString = "".join([art for art in real])
+
+wc = WordCloud(stopwords=STOPWORDS, height=1080, width=1920)
+
+r_wc = wc.generate(realString)
+
+r_wc.to_file('static/visualizations/realWordCloud.png')
