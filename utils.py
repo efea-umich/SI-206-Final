2ef0f27366297ab5f1f0f4eb74f7f64eb262d1f2
Michael Zhou <mzhou08@gmail.com>
2021-04-17 00:07:56 -0400

    finished up the utils doc. Cleaned all data and wrote in documentation for the code.

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import re
 
 
 def removeLinks(text):
-    return re.sub("[^\.][A-Za-z\.\/:]+(?:\.com|\.org|\.gov|\.us|\.edu|\.net|\.co|\.be)\/[\w\/\?\-]+", "", text)
+    return re.sub("[^\.][A-Za-z\.\/:]+(?:\.com|\.org|\.gov|\.us|\.edu|\.net|\.co|\.be)(?:\/[\w\/\?\-\!\#]+)?", "", text)
 
 def removeFeaturedVia(text):
     #return re.sub('(\w+)\.(\w+)', '\1. \2', text)
@@ -22,8 +22,12 @@ f["text"] = f["text"].apply(removeLinks).apply(removeFeaturedVia).apply(fixConct
 f.to_csv('static/Fake_Cleaned.csv')
 
 def removeArticleStart(text):
-    return re.sub('^.+ \(\w+\) - ', "", text)
+    return re.sub('^(?:.+ )?\(\w+\) - ', "", text)
+
+def removeReuters(text):
+    return re.sub('.?(?:Reuters|REUTERS)(?:\/\w+.|.)', " ", text)
 
 t = pd.read_csv('static/True.csv')
-t["text"] = t["text"].apply(removeArticleStart).apply(removeLinks).apply(removeFeaturedVia).apply(fixConctracs).apply(removeMultSpc)
+t["text"] = t["text"].apply(removeArticleStart).apply(removeReuters).apply(removeLinks).apply(removeFeaturedVia).apply(fixConctracs).apply(removeMultSpc)
+t["title"] = t["title"].apply(removeReuters).apply(removeMultSpc)
 t.to_csv('static/True_Cleaned.csv')
