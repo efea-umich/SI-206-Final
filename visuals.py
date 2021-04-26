666fddc357c5b760863a761c9c079818b0ae55c2
Michael Zhou <mzhou08@gmail.com>
2021-04-26 12:13:30 -0400

    added new JOIN statement for the function most_commented.

--- visuals.py
+++ visuals.py
@@ -19,6 +19,20 @@ def percentRF(db):
 
     return percentR, percentF
 
+def mostCommented():
+
+    postList = []
+
+    cur.execute("""SELECT Reddit.title, The_Onion.pred, The_Onion.predVal FROM Reddit 
+    JOIN The_ONION ON Reddit.link = Onion.link 
+    ORDER BY Reddit.num_comments DESC""")
+
+    for c in cur:
+        postList.append(c)
+
+    return postList[:20]
+
 
 print(avgPredVal('CNN'))
 print(percentRF('CNN'))
+print(mostCommented())
